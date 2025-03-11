d79455f1724b8fd7457b3250fe943af6f2cc0614
Jeremy Fowler <jembyfowler@gmail.com>
2025-03-11 10:32:58 -0600

    kicked values down the stairs

--- src/db/DbContext.js
+++ src/db/DbContext.js
@@ -1,9 +1,7 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account.js'
-import { ValueSchema } from '../models/Value.js'
 
 class DbContext {
-  Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
 }
 
