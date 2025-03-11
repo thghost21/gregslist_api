05a31930a8977d1f26ff3829ac0051ab8b3af714
thghost21 <tylerhacker21@gmail.com>
2025-03-11 14:34:02 -0600

    Get all

---
+++ src/services/HousesService.js
@@ -0,0 +1,10 @@
+import { dbContext } from "../db/DbContext.js"
+
+class HousesService {
+  async getAllHouses() {
+    const houses = await dbContext.Houses.find()
+    return houses
+  }
+
+}
+export const housesService = new HousesService()
