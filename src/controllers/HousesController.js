9e7a133c24b05bac670d66d6fbb6c942b22b7cba
thghost21 <tylerhacker21@gmail.com>
2025-03-11 14:44:39 -0600

    get by id

--- src/controllers/HousesController.js
+++ src/controllers/HousesController.js
@@ -6,11 +6,26 @@ export class HousesController extends BaseController {
     super('api/houses')
     this.router
       .get('', this.getAllHouses)
+      .get('/:houseId', this.getHouseById)
   }
   async getAllHouses(request, response, next) {
     try {
       const houses = await housesService.getAllHouses()
       response.send(houses)
+    } catch (error) {
+      next(error)
+    }
+  }
+  /**
+   * @param {import("express").Request} request
+   * @param {import("express").Response} response
+   * @param {import("express").NextFunction} next
+   */
+  async getHouseById(request, response, next) {
+    try {
+      const houseId = request.params.houseId
+      const house = await housesService.getHouseById(houseId)
+      response.send(house)
     } catch (error) {
       next(error)
     }
--- src/services/HousesService.js
+++ src/services/HousesService.js
@@ -1,9 +1,17 @@
+import { BadRequest } from "@bcwdev/auth0provider/lib/Errors.js";
 import { dbContext } from "../db/DbContext.js"
 
 class HousesService {
   async getAllHouses() {
     const houses = await dbContext.Houses.find()
     return houses
+  }
+  async getHouseById(houseId) {
+    const house = await dbContext.Houses.findById(houseId)
+    if (house == null) {
+      throw new BadRequest(`${houseId} is not a valid car id!`);
+
+    } return house
   }
 
 }
