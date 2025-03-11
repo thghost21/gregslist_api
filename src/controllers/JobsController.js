41c0608368e95b1767060f33c60c421dcc19be82
thghost21 <tylerhacker21@gmail.com>
2025-03-11 16:41:53 -0600

    jobs working

--- src/controllers/JobsController.js
+++ src/controllers/JobsController.js
@@ -6,10 +6,30 @@ export class JobsController extends BaseController {
     super('api/jobs')
     this.router
       .get('', this.getJobs)
+      .get('/search', this.getJobByQuery)
+      .get('/:jobId', this.getJobById)
   }
   async getJobs(request, response, next) {
     try {
       const jobs = await jobsService.getJobs()
+      response.send(jobs)
+    } catch (error) {
+      next(error)
+    }
+  }
+  async getJobById(request, response, next) {
+    try {
+      const jobId = request.params.jobId
+      const job = await jobsService.getJobById(jobId)
+      response.send(job)
+    } catch (error) {
+      next(error)
+    }
+  }
+  async getJobByQuery(request, response, next) {
+    try {
+      const jobQuery = request.query
+      const jobs = await jobsService.getJobByQuery(jobQuery)
       response.send(jobs)
     } catch (error) {
       next(error)
--- src/services/JobsService.js
+++ src/services/JobsService.js
@@ -1,9 +1,22 @@
+import { BadRequest } from "@bcwdev/auth0provider/lib/Errors.js";
 import { dbContext } from "../db/DbContext.js"
 
 class JobsService {
   async getJobs() {
     const jobs = await dbContext.Jobs.find()
     return jobs
+  }
+  async getJobById(jobId) {
+    const job = await dbContext.Jobs.findById(jobId)
+    if (job == null) {
+      throw new BadRequest("Could not find a job with the id of " + jobId);
+    } return job
+  }
+  async getJobByQuery(jobQuery) {
+    const sortBy = jobQuery.sortBy
+    delete jobQuery.sortBy
+    const job = await dbContext.Jobs.find(jobQuery).sort(sortBy)
+    return job
   }
 }
 
