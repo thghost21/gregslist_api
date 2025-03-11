31d18d4621914a3232996457e1c1fda37097c576
Jeremy Fowler <jembyfowler@gmail.com>
2025-03-11 10:41:15 -0600

    added test data and registered schema

--- src/db/DbContext.js
+++ src/db/DbContext.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account.js'
+import { CarSchema } from '../models/Car.js';
 
 class DbContext {
   Account = mongoose.model('Account', AccountSchema);
+  Cars = mongoose.model('Car', CarSchema)
 }
 
 export const dbContext = new DbContext()
